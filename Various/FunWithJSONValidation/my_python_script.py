018dd3a34a1b10207566b830c3bd703df0a83539
Ebbe Melo Sørensen <ebs@dmi.dk>
2025-06-03 11:04:16 +0200

    -

--- Various/FunWithJSONValidation/my_python_script.py
+++ Various/FunWithJSONValidation/my_python_script.py
@@ -5,7 +5,24 @@
 #import geopandas
 import json
 import jsonschema
+from jsonschema import validate
 from lxml import etree
+
+def validate_json(json_file_path, schema_file_path):
+    # Load the schema
+    with open(schema_file_path, 'r') as schema_file:
+        schema = json.load(schema_file)
+
+    # Load the data
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    try:
+        validate(instance=data, schema=schema)
+        print(f"✅ JSON is valid according to the schema: {json_file_path}")
+    except jsonschema.exceptions.ValidationError as err:
+        print(f"❌ JSON is invalid: {json_file_path}")
+        print("Error:", err.message)
 
 def validate_xml(xml_file_path, xsd_file_path):
     try:
@@ -33,17 +50,18 @@ if __name__ == '__main__':
     print("Fun with xml validation")
 
     # First, I'm just gonna read a json file
-    with open('myjsonfile.json', 'r', encoding='utf8') as f:
-        json_document = json.load(f)
+    # with open('myjsonfile.json', 'r', encoding='utf8') as f:
+    #     json_document = json.load(f)
 
-    if False:
-        print(json_document)
+    # # Then I'm gonna validate it against the json schema
+    # with open('schema_manually_corrected.json', 'r', encoding='utf8') as f:
+    #     json_schema = json.load(f)
 
-    # Then I'm gonna validate it against the json schema
-    with open('schema_manually_corrected.json', 'r', encoding='utf8') as f:
-        json_schema = json.load(f)
+    # Also gonna try with xml
+    validate_xml('books.xml', 'books.xsd')
 
-    validate_xml('myjsonfile.json', 'schema_manually_corrected.json')
+    json_schema_file = 'schema_manually_corrected.json'
 
-
-
+    validate_json('creatures_valid.json', json_schema_file)
+    validate_json('creatures_invalid_enum_out_of_range.json', json_schema_file)
+    validate_json('creatures_invalid_required_property_missing.json', json_schema_file)    
