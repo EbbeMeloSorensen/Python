e8840067921157acddae05217f2bdc1b46012375
Ebbe Melo Sørensen <ebs@dmi.dk>
2026-03-22 16:47:59 +0100

    -

--- Various/my_project/src/my_module.py
+++ Various/my_project/src/my_module.py
@@ -1,2 +1,5 @@
+"""Arithmetic functions module."""
+
 def add(a: int, b: int) -> int:
+    """Function for adding numbers."""
     return a + b
--- Various/my_project/tests/test_my_module.py
+++ Various/my_project/tests/test_my_module.py
@@ -1,7 +1,11 @@
+"""Unit tests for my_module."""
+
 from src.my_module import add
 
 def test_add_positive_numbers():
+    """Test for adding positive numbers."""
     assert add(2, 3) == 5
 
 def test_add_negative_numbers():
+    """Test for adding negative numbers."""
     assert add(-1, -1) == -2
